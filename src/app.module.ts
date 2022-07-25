aa2f0aea6c7981f7e478345533f53792b4cd05d5
Kinetic639 <michal.stepien36@gmail.com>
2022-07-25 11:14:46 +0200

    nest setup adn typeorm configuration

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import {AppDataSource} from "../ormconfig";
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+      ConfigModule.forRoot(),
+      TypeOrmModule.forRoot(AppDataSource.options)
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
